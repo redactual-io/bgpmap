067cb3b868e5d524704f095158af3f725e05ff9c
Matt Finn <mafinn@cisco.com>
2017-01-09 20:57:17 -0600

    initial script upload

---
+++ bgpmap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+mrt_file=$1
+ref_as=$2
+help () {
+	local err_msg=$1
+	echo -e "\t\t$0 <bgp table file> <reference ASN>"
+	if [[ ${err_msg:-null} != "null" ]]; then
+		echo -e "\t\tERROR: $err_msg"
+	fi
+	exit
+}
+if [[ ${mrt_file:-null} == "null" ]]; then
+	help "missing bgp table file"
+elif [[ ! -f ${mrt_file} ]]; then
+	help "file does not exist" 		
+elif [[ ${ref_as:-null} == "null" ]]; then
+	help "provide reference ASN of bgp table file"
+fi
+#grab the file name without the extension
+fname=$(cut -d . -f1 <<< $mrt_file)
+# dump the MRT format BGP table
+bgpdump $mrt_file |\
+# extract only the ASPATH's from each entry
+sed -rn 's/ASPATH:\ //gp' |\
+# remove empty lines
+grep -v "^\s*$" |\
+# insert the ref_as at the beginning of each path
+# remove aggregate as-sets
+# collapse path prepending
+# convert to CSV format
+# remove spaces and tabs
+perl -pe "s/^/$ref_as\ /g;\
+s/^(([0-9]+\s)+)(\{[0-9,]+\})/\1/g;\
+s/((\b[0-9]+\b)\s)(\b\1\b)*\b\2\b/\2/g;\
+s/(\d)\s+(\d)/\1,\2/g;\
+s/(\ |\t)//g" |\
+# split the output for multithreaded processing and generate edges
+parallel --pipe ./edge_gen.sh |\
+# collect edge output and deduplicate 
+sort -u -t , -k1,1n -k2,2n |\
+pigz --fast > $fname-BASE.gz
---
+++ edge_gen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Create edges between nodes represented in the CSV ASPATH
+# e.g "10,34,106,45782" will become:
+#	"10 34
+#	 34 106
+#	 106 45782"
+#
+perl -pe 's/((?<!^)\b[0-9]+\b(?<!$))/\1|\1/g;s/\|/\n/g' |\
+
+#
+# ensure the lower ASN is always on the left
+#
+awk -F',' '{if ($1 < $2) {print $1","$2} else {print $2","$1}}' |\
+
+#
+# collapse duplicate entries and sort numerically
+#
+sort -u -t , -k1,1n -k2,2n
